596ec068f1d9fa510151ce08e89df8d5a1fd2b86
Eli Sklar <eli.sklar@gmail.com>
2016-05-19 01:26:18 +0300

    basic crude material functionality

---
+++ client/app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jasonApp', ['jasonApp.constants', 'ngMaterial', 'ngCookies', 'ngResource', 'ngSanitize',
+    'ui.router'
+  ])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
